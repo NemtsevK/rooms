3e86c6bfe5119e8fccdba944f4b48fc16f13738a
Konstantin <mutecross@gmail.com>
2023-05-24 00:24:40 +0300

    классы по бэм
    классы переписаны по бэм, стили объеденены в один, удалена папка со шрифтами.

--- js/ext.js
+++ js/ext.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-    $(".card").on("mouseover",function () {
-        if(!$(this).children(".card__reserved:visible").length && $(this).children(".best_price.active").length){
+    $(".card").on("mouseover", function () {
+        if (!$(this).children(".card__reserved:visible").length && $(this).children(".best_price.active").length) {
             $(this).children(".best_price.active").fadeIn("200");
-            $(".card").on("mouseleave",function () {
+            $(".card").on("mouseleave", function () {
                 $(this).children(".best_price.active").fadeOut("200");
             });
         }
     });
 
-    $(".card").find(".button").on("click",function () {
-        $(this).parents(".card").on("mouseleave",function () {
+    $(".card").find(".button").on("click", function () {
+        $(this).parents(".card").on("mouseleave", function () {
             $(this).find(".card__reserved, .footer__reserved").fadeIn("200");
             $(this).find(".best_price.active").fadeOut("200");
             $(".card").off("mouseleave");
         });
     });
 
-    $(".card").on("click",".button",function(){
+    $(".card").on("click", ".button", function () {
         return false;
-    }).on("click",function () {
+    }).on("click", function () {
         $(".card").find(".card__reserved, .footer__reserved").fadeOut("200");
     });
 
