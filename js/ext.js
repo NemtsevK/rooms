2734febf9a5525025b13003c2787823c77565301
Konstantin <mutecross@gmail.com>
2023-05-28 21:42:21 +0300

    js исправлен

--- js/ext.js
+++ js/ext.js
@@ -2,34 +2,29 @@ $(document).ready(function () {
     $('.card-wrapper--active')
         .find('.card__button')
         .on('click', ClickButton);
-
-    $('.card-wrapper--selected')
-        .on('click', '.card__button', function () {return false;})
-        .on('click', CancelReserved);
 });
 
 function ClickButton() {
+    console.log('click-active');
     $(this)
         .parents('.card-wrapper')
         .on('mouseleave', SetReserved);
 }
 
 function SetReserved() {
+    console.log('mouseleave-active');
     $(this)
         .off('mouseleave')
         .removeClass('card-wrapper--active')
         .addClass('card-wrapper--selected')
-        .find('.card-wrapper__overlay, .card-wrapper__reserved-wrapper')
-        .fadeIn('200',function (){
-            $(this).on('click', CancelReserved);
-        });
+        .off('click')
+        .on('click', CancelReserved);
 }
 
 function CancelReserved() {
+    console.log('click-selected');
     $(this)
-        .parents('.card-wrapper--selected')
+        .off('click')
         .removeClass('card-wrapper--selected')
         .addClass('card-wrapper--active')
-        .find('.card-wrapper__overlay, .card-wrapper__reserved-wrapper')
-        .fadeOut('200');
 }
