baa850f3facbc214beda451ffe4c910081d40d0e
Konstantin <mutecross@gmail.com>
2023-05-28 18:08:35 +0300

    изменён css

--- js/ext.js
+++ js/ext.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         .find('.card__button')
         .on('click', ClickButton);
 
-    $('.card-wrapper:not(.card-wrapper--active)')
+    $('.card-wrapper--selected')
         .on('click', '.card__button', function () {return false;})
         .on('click', CancelReserved);
 });
@@ -11,12 +11,7 @@ $(document).ready(function () {
 function ClickButton() {
     $(this)
         .parents('.card-wrapper')
-        .on({
-            'mouseleave': SetReserved,
-            'touchend': SetReserved,
-        });
-    // $(this).parents('.card-wrapper').on('touchend ', SetReserved);
-
+        .on('mouseleave', SetReserved);
 }
 
 function SetReserved() {
@@ -31,13 +26,10 @@ function SetReserved() {
 }
 
 function CancelReserved() {
-    console.log($(this));
     $(this)
         .parents('.card-wrapper--selected')
         .removeClass('card-wrapper--selected')
         .addClass('card-wrapper--active')
         .find('.card-wrapper__overlay, .card-wrapper__reserved-wrapper')
         .fadeOut('200');
-
-    // }
 }
