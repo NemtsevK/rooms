0d5a1b7cbe2621fdf921cc4c5de18166df66694a
Konstantin <mutecross@gmail.com>
2023-06-01 00:15:02 +0300

    новые исправления, шрифты

--- js/ext.js
+++ js/ext.js
@@ -1,27 +1,27 @@
-$(document).ready(function () {
-    $('.card-wrapper--active')
-        .find('.card__button')
-        .on('click', ClickButton);
-});
+let card_button = document.querySelectorAll('.card-wrapper--active .card__button');
+console.log(card_button);
+card_button.addEventListener('click', ClickButton,false);
 
 function ClickButton() {
-    $(this)
-        .parents('.card-wrapper')
-        .on('mouseleave', SetReserved);
+    console.log('click');
+    let card_wrapper = card_button[0].closest('.card-wrapper');
+    card_wrapper.addEventListener('mouseleave', SetReserved, false);
+
+    function SetReserved() {
+        console.log('reserved');
+        card_wrapper.classList.remove('card-wrapper--active');
+        card_wrapper.classList.add('card-wrapper--selected');
+        card_wrapper.addEventListener('click', CancelReserved,false);
+
+        function CancelReserved() {
+            console.log('cancel_reserved');
+            card_wrapper.classList.remove('card-wrapper--selected');
+            card_wrapper.classList.add('card-wrapper--active');
+        }
+    }
+
 }
 
-function SetReserved() {
-    $(this)
-        .off('mouseleave')
-        .removeClass('card-wrapper--active')
-        .addClass('card-wrapper--selected')
-        .off('click')
-        .on('click', CancelReserved);
-}
 
-function CancelReserved() {
-    $(this)
-        .off('click')
-        .removeClass('card-wrapper--selected')
-        .addClass('card-wrapper--active')
-}
+
+
